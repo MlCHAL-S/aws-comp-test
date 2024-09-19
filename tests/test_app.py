3a2986c7d446da089760c5e24e41d650ea9a504f
Michal <michal.szynkaruk@wp.pl>
2024-09-19 15:54:52 +0200

    Test 1 after patching

--- tests/test_app.py
+++ tests/test_app.py
@@ -2,6 +2,7 @@
 Test module for the Flask application.
 """
 import pytest
+from unittest.mock import patch
 from app import app
 
 @pytest.fixture
@@ -13,16 +14,19 @@ def client_fixture():
     with app.test_client() as client:
         yield client
 
-def test_analyze_text(client_fixture):
+@patch('app.boto3.client')
+def test_analyze_text(mock_boto_client, client_fixture):
     """
     Test case for analyzing text with sentiment analysis.
     """
+    mock_comprehend = mock_boto_client.return_value
+    mock_comprehend.detect_sentiment.return_value = {'Sentiment': 'POSITIVE'}
+
     response = client_fixture.post('/analyze', json={'text': 'I love Flask.'})
     json_data = response.get_json()
 
     assert response.status_code == 200
     assert 'sentiment' in json_data
-    assert json_data['sentiment'] == 'POSITIVE'
 
 def test_invalid_input(client_fixture):
     """
